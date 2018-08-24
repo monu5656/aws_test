d47d3f28f50016d8ba8a433a8c4458b764929677
Monu <monuburnwal786@gmail.com>
2018-08-24 10:32:45 +0530

    commit

--- tdd_final/src/test/java/TddProject/Test_final.java
+++ tdd_final/src/test/java/TddProject/Test_final.java
@@ -8,7 +8,7 @@ public class Test_final {
 
 	@Test
 	public void f1(){
-		System.setProperty("webdriver.gecko.driver","//usr//local//bin//geckodriver");
+		System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
 		WebDriver dd = new FirefoxDriver();
 		dd.get("http://52.74.156.183:8090/Spring3HibernateApp-0.0.1-SNAPSHOT/");
 		System.out.print("mera name");
