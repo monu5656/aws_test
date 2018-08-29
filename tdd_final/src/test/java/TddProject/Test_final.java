a1d3742e2ea6e56c33019166b47f370a52cdc366
monu68669 <monuburnwal786@gmail.com>
2018-08-29 18:12:56 +0530

    Update Test_final.java

--- tdd_final/src/test/java/TddProject/Test_final.java
+++ tdd_final/src/test/java/TddProject/Test_final.java
@@ -14,10 +14,15 @@ public class Test_final {
 	@Test
 	public void f1(){
 		
-		WebDriver driver = new FirefoxDriver();
-		System.out.print("mera name");
-		System.setProperty("webdriver.gecko.driver","/geckodriver");
 		
+		System.out.print("mera name");
+		System.setProperty("webdriver.gecko.driver","/monu_tom/geckodriver");
+		File pathBinary = new File("/usr/bin/firefox");
+		FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);   
+		DesiredCapabilities desired = DesiredCapabilities.firefox();
+		FirefoxOptions options = new FirefoxOptions();
+		desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
+		WebDriver driver = new FirefoxDriver(options);
 		driver.get("http://52.74.156.183:8070/Spring3HibernateApp-0.0.1-SNAPSHOT/");
 	}
 	
